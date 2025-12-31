8d3acabf04e2ae71a69f11e3884738e24c2f7c2f
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-12-31 08:32:57 -0500

    new plugins

--- go.mod
+++ go.mod
@@ -1,13 +1,7 @@
 module github.com/kgarner7/navidrome-listenbrainz-plugin
 
-go 1.24.5
+go 1.25
 
-toolchain go1.24.6
+require github.com/extism/go-pdk v1.1.3
 
-require github.com/navidrome/navidrome v0.58.0
-
-require (
-	github.com/knqyf263/go-plugin v0.9.0 // indirect
-	github.com/tetratelabs/wazero v1.9.0 // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
-)
+require github.com/navidrome/navidrome/plugins/pdk/go v0.0.0-20251231034047-0f5f67f49954
--- plugin.go
+++ plugin.go
@@ -3,70 +3,46 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
 
-	"github.com/navidrome/navidrome/plugins/api"
-	"github.com/navidrome/navidrome/plugins/host/http"
+	"github.com/extism/go-pdk"
+	"github.com/navidrome/navidrome/plugins/pdk/go/metadata"
 )
 
 const (
-	lbzEndpoint  = "https://api.listenbrainz.org/1/"
-	lbzTimeoutMs = 5000
+	lbzEndpoint = "https://api.listenbrainz.org/1/"
 
-	labsBase    = "https://labs.api.listenbrainz.org/"
-	labsTimeout = 10000
-	algorithm   = "session_based_days_9000_session_300_contribution_5_threshold_15_limit_50_skip_30"
+	labsBase  = "https://labs.api.listenbrainz.org/"
+	algorithm = "session_based_days_9000_session_300_contribution_5_threshold_15_limit_50_skip_30"
 )
 
-var (
-	client = http.NewHttpService()
-)
+var notFound = errors.New("not found")
 
 type ListenBrainzAgent struct{}
 
-func listenBrainzRequest(ctx context.Context, endpoint string, params url.Values) ([]byte, error) {
+// Ensure wikimediaPlugin implements the provider interfaces
+var (
+	_ metadata.ArtistURLProvider      = (*ListenBrainzAgent)(nil)
+	_ metadata.ArtistTopSongsProvider = (*ListenBrainzAgent)(nil)
+	_ metadata.SimilarArtistsProvider = (*ListenBrainzAgent)(nil)
+)
+
+func listenBrainzRequest(endpoint string, params url.Values) ([]byte, error) {
 	url := fmt.Sprintf("%s%s?%s", lbzEndpoint, endpoint, params.Encode())
-	httpReq := &http.HttpRequest{
-		Url: url,
-		Headers: map[string]string{
-			"Accept":     "application/json",
-			"User-Agent": "NavidromeListenBrainzPlugin/0.1",
-		},
-		TimeoutMs: lbzTimeoutMs,
+	req := pdk.NewHTTPRequest(pdk.MethodGet, url)
+	req.SetHeader("Accept", "application/json")
+	req.SetHeader("User-Agent", "NavidromeListenBrainzPlugin/0.1")
+
+	resp := req.Send()
+
+	if resp.Status() != 200 {
+		return nil, fmt.Errorf("ListenBrainz HTTP error: status %d, body: %s", resp.Status, string(resp.Body()))
 	}
 
-	resp, err := client.Get(ctx, httpReq)
-	if err != nil {
-		return nil, fmt.Errorf("ListenBrainz request error: %w", err)
-	}
-	if resp.Status != 200 {
-		return nil, fmt.Errorf("ListenBrainz HTTP error: status %d, body: %s", resp.Status, string(resp.Body))
-	}
-
-	return resp.Body, nil
-}
-
-func (l ListenBrainzAgent) GetAlbumImages(context.Context, *api.AlbumImagesRequest) (*api.AlbumImagesResponse, error) {
-	return nil, api.ErrNotImplemented
-}
-
-func (l ListenBrainzAgent) GetAlbumInfo(context.Context, *api.AlbumInfoRequest) (*api.AlbumInfoResponse, error) {
-	return nil, api.ErrNotImplemented
-}
-
-func (l ListenBrainzAgent) GetArtistBiography(context.Context, *api.ArtistBiographyRequest) (*api.ArtistBiographyResponse, error) {
-	return nil, api.ErrNotImplemented
-}
-
-func (l ListenBrainzAgent) GetArtistImages(context.Context, *api.ArtistImageRequest) (*api.ArtistImageResponse, error) {
-	return nil, api.ErrNotImplemented
-}
-
-func (l ListenBrainzAgent) GetArtistMBID(context.Context, *api.ArtistMBIDRequest) (*api.ArtistMBIDResponse, error) {
-	return nil, api.ErrNotImplemented
+	return resp.Body(), nil
 }
 
 type trackInfo struct {
@@ -74,33 +50,33 @@ type trackInfo struct {
 	RecordingMbid string `json:"recording_mbid"`
 }
 
-func (l ListenBrainzAgent) GetArtistTopSongs(ctx context.Context, req *api.ArtistTopSongsRequest) (*api.ArtistTopSongsResponse, error) {
-	if req.Mbid == "" {
-		return nil, api.ErrNotFound
+func (l ListenBrainzAgent) GetArtistTopSongs(req metadata.TopSongsRequest) (metadata.TopSongsResponse, error) {
+	if req.MBID == "" {
+		return metadata.TopSongsResponse{}, notFound
 	}
 
-	resp, err := listenBrainzRequest(ctx, "popularity/top-recordings-for-artist/"+req.Mbid, url.Values{})
+	resp, err := listenBrainzRequest("popularity/top-recordings-for-artist/"+req.MBID, url.Values{})
 	if err != nil {
-		return nil, err
+		return metadata.TopSongsResponse{}, err
 	}
 
 	var tracks []trackInfo
 	if err := json.Unmarshal(resp, &tracks); err != nil {
-		return nil, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
+		return metadata.TopSongsResponse{}, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
 	}
 
 	// Make sure we do not exceed the number of requested songs.
 	count := min(len(tracks), int(req.Count))
 
-	songs := make([]*api.Song, count)
+	songs := make([]metadata.SongRef, count)
 	for idx, track := range tracks[:count] {
-		songs[idx] = &api.Song{
-			Mbid: track.RecordingMbid,
+		songs[idx] = metadata.SongRef{
+			MBID: track.RecordingMbid,
 			Name: track.RecordingName,
 		}
 	}
 
-	return &api.ArtistTopSongsResponse{Songs: songs}, nil
+	return metadata.TopSongsResponse{Songs: songs}, nil
 }
 
 type artistMetadataResult struct {
@@ -109,33 +85,33 @@ type artistMetadataResult struct {
 	} `json:"rels,omitzero"`
 }
 
-func (l ListenBrainzAgent) GetArtistURL(ctx context.Context, req *api.ArtistURLRequest) (*api.ArtistURLResponse, error) {
-	if req.Mbid == "" {
-		return nil, api.ErrNotFound
+func (l ListenBrainzAgent) GetArtistURL(req metadata.ArtistRequest) (metadata.ArtistURLResponse, error) {
+	if req.MBID == "" {
+		return metadata.ArtistURLResponse{}, notFound
 	}
 
 	params := url.Values{}
-	params.Add("artist_mbids", req.Mbid)
+	params.Add("artist_mbids", req.MBID)
 
-	resp, err := listenBrainzRequest(ctx, "metadata/artist", params)
+	resp, err := listenBrainzRequest("metadata/artist", params)
 	if err != nil {
-		return nil, err
+		return metadata.ArtistURLResponse{}, err
 	}
 
 	var result []artistMetadataResult
 	if err := json.Unmarshal(resp, &result); err != nil {
-		return nil, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
+		return metadata.ArtistURLResponse{}, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
 	}
 
 	if len(result) != 1 {
-		return nil, api.ErrNotFound
+		return metadata.ArtistURLResponse{}, notFound
 	}
 
 	if result[0].Rels.OfficialHomepage != "" {
-		return &api.ArtistURLResponse{Url: result[0].Rels.OfficialHomepage}, nil
+		return metadata.ArtistURLResponse{URL: result[0].Rels.OfficialHomepage}, nil
 	}
 
-	return nil, api.ErrNotFound
+	return metadata.ArtistURLResponse{}, notFound
 }
 
 type artist struct {
@@ -143,46 +119,39 @@ type artist struct {
 	Name string `json:"name"`
 }
 
-func (l ListenBrainzAgent) GetSimilarArtists(ctx context.Context, req *api.ArtistSimilarRequest) (*api.ArtistSimilarResponse, error) {
-	if req.Mbid == "" {
-		return nil, api.ErrNotFound
+func (l ListenBrainzAgent) GetSimilarArtists(req metadata.SimilarArtistsRequest) (metadata.SimilarArtistsResponse, error) {
+	if req.MBID == "" {
+		return metadata.SimilarArtistsResponse{}, notFound
 	}
 
-	url := fmt.Sprintf("%ssimilar-artists/json?artist_mbids=%s&algorithm=%s", labsBase, req.Mbid, algorithm)
-	httpReq := &http.HttpRequest{
-		Url: url,
-		Headers: map[string]string{
-			"Accept":     "application/json",
-			"User-Agent": "NavidromeListenBrainzPlugin/0.1",
-		},
-		TimeoutMs: labsTimeout,
-	}
+	url := fmt.Sprintf("%ssimilar-artists/json?artist_mbids=%s&algorithm=%s", labsBase, req.MBID, algorithm)
+	httpReq := pdk.NewHTTPRequest(pdk.MethodGet, url)
+	httpReq.SetHeader("Accept", "application/json")
+	httpReq.SetHeader("User-Agent", "NavidromeListenBrainzPlugin/0.1")
 
-	resp, err := client.Get(ctx, httpReq)
-	if err != nil {
-		return nil, fmt.Errorf("ListenBrainz labs request error: %w", err)
-	}
-	if resp.Status != 200 {
-		return nil, fmt.Errorf("ListenBrainz labs HTTP error: status %d, body: %s", resp.Status, string(resp.Body))
+	resp := httpReq.Send()
+
+	if resp.Status() != 200 {
+		return metadata.SimilarArtistsResponse{}, fmt.Errorf("ListenBrainz labs HTTP error: status %d, body: %s", resp.Status, string(resp.Body()))
 	}
 
 	var lbzArtists []artist
-	if err := json.Unmarshal(resp.Body, &lbzArtists); err != nil {
-		return nil, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
+	if err := json.Unmarshal(resp.Body(), &lbzArtists); err != nil {
+		return metadata.SimilarArtistsResponse{}, fmt.Errorf("failed to parse ListenBrainz response: %w", err)
 	}
 
 	// Make sure we do not exceed the number of requested songs.
 	count := min(len(lbzArtists), int(req.Limit))
 
-	artists := make([]*api.Artist, count)
+	artists := make([]metadata.ArtistRef, count)
 	for i, artist := range lbzArtists[:count] {
-		artists[i] = &api.Artist{
-			Mbid: artist.MBID,
+		artists[i] = metadata.ArtistRef{
+			MBID: artist.MBID,
 			Name: artist.Name,
 		}
 	}
 
-	return &api.ArtistSimilarResponse{
+	return metadata.SimilarArtistsResponse{
 		Artists: artists,
 	}, nil
 }
@@ -190,5 +159,5 @@ func (l ListenBrainzAgent) GetSimilarArtists(ctx context.Context, req *api.Artis
 func main() {}
 
 func init() {
-	api.RegisterMetadataAgent(ListenBrainzAgent{})
+	metadata.Register(&ListenBrainzAgent{})
 }
